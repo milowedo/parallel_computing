9235ed4398b20d7af213b5023a023e1a6ef3e035
Milosz Szwedo <plgmilowedo@zeus.cyfronet.pl>
2020-03-24 23:32:33 +0100

    added configuration on how to run multiple jobs, also some utils to join results

--- monteCarlo_pi/runMany.sh
+++ monteCarlo_pi/runMany.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #arg1 ilosc watkow
 #arg2 ilosc punkto
-#mpicc -o carlo paralell_Carlo.c -lm
+mpicc -o carlo paralell_Carlo.c -lm
 
 #Uruchom program ze zmieniającą się liczbą wątków, od 1 do 12.
 
 # Każde uruchomienie powinno policzyć w sumie tyle samo punktów - rozmiar problemu pozostaje stały, praca jednego procesora maleje (eksperyment nieskalowany)
-# for ((i=1; i<=$1; i++)); do
-#     mpiexec --hostfile allnodes -np $i ./carlo $2
-# done
+ for ((i=1; i<=$1; i++)); do
+     mpiexec -np $i ./carlo $2
+ done
 
 # Za każdym razem każdy procesor powinien liczyć tyle samo punktów - rozmiar problemu rośnie, praca jednego procesora pozostaje stała (eksperyment skalowany)
 # arg2 = 0
 for ((i=1; i<=$1; i++)); do
-    mpiexec --hostfile allnodes -np $i ./carlo $2 0
+    mpiexec -np $i ./carlo $2 0
 done
 
 
