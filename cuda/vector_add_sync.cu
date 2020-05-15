c75b876f5173c0e0f37bc8331a7976b1a99d2b54
milowedo <miloszszwedo@gmail.com>
2020-05-15 20:31:11 +0200

    Added arguments

--- cuda/vector_add_sync.cu
+++ cuda/vector_add_sync.cu
@@ -14,6 +14,26 @@ void add(int* a, int* b, int* c, int N)
 
 int main(void)
 {
+    if (argc != 4)
+    {
+        fprintf(stderr, "You have to provide ARRAY_SIZE, THREAD_COUNT, BLOCK_COUNT and  as arguments.\n");
+        return -1;
+    }
+    
+    char *p;
+
+    int ARRAY_SIZE;
+    ul_conv = strtoul(argv[1], &p, 10);
+    ARRAY_SIZE = ul_conv;
+
+    int THREAD_COUNT;
+    ul_conv = strtoul(argv[2], &p, 10);
+    THREAD_COUNT = ul_conv;
+
+    int BLOCK_COUNT;
+    ul_conv = strtoul(argv[3], &p, 10);
+    BLOCK_COUNT = ul_conv;
+
     int N = 10;
     int* a = (int *)malloc(N * sizeof(int));
     int* b = (int *)malloc(N * sizeof(int));
