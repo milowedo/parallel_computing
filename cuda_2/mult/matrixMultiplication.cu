6f08df4c8bc60db7bda89871edb9a617c46d7325
milowedo <miloszszwedo@gmail.com>
2020-05-22 15:39:03 +0200

    Added time measuring and program arguments

--- cuda_2/mult/matrixMultiplication.cu
+++ cuda_2/mult/matrixMultiplication.cu
@@ -8,7 +8,7 @@ using namespace std;
 #define BLOCK_SIZE 16
 
 typedef struct
-{	int width;
+{	int WIDTH;
 	int height;
 	float *elements;
 } Matrix;
@@ -21,26 +21,26 @@ void MatMul(const Matrix A, const Matrix B, Matrix C)
 {
 	// Load matrices A and B to device memory
 	Matrix d_A;
-	d_A.width = A.width; d_A.height = A.height;
-	size_t size = A.width * A.height * sizeof(float);
+	d_A.WIDTH = A.WIDTH; d_A.height = A.height;
+	size_t size = A.WIDTH * A.height * sizeof(float);
 	cudaMalloc((void**) &d_A.elements, size);
 	cudaMemcpy(d_A.elements, A.elements, size, cudaMemcpyHostToDevice);
 	
 	Matrix d_B;
-	d_B.width = B.width; d_B.height = B.height;
-	size = B.width * B.height * sizeof(float);
+	d_B.WIDTH = B.WIDTH; d_B.height = B.height;
+	size = B.WIDTH * B.height * sizeof(float);
 	cudaMalloc((void**) &d_B.elements, size);
 	cudaMemcpy(d_B.elements, B.elements, size, cudaMemcpyHostToDevice);
 	
 	// allocate C in device
 	Matrix d_C;
-	d_C.width = C.width; d_C.height = C.height;
-	size = d_C.width * d_C.height * sizeof(float);
+	d_C.WIDTH = C.WIDTH; d_C.height = C.height;
+	size = d_C.WIDTH * d_C.height * sizeof(float);
 	cudaMalloc((void**) &d_C.elements, size);
 	
 	// Invoke kernel
     dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
-    dim3 dimGrid(B.width / dimBlock.x, A.height / dimBlock.y);
+    dim3 dimGrid(B.WIDTH / dimBlock.x, A.height / dimBlock.y);
     MatMulKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C);
 	
 	// copy C to host
@@ -59,27 +59,27 @@ __global__ void MatMulKernel(Matrix A, Matrix B, Matrix C)
 	 float Cvalue = 0;
 	 int row = blockIdx.y * blockDim.y + threadIdx.y;
 	 int col = blockIdx.x * blockDim.x + threadIdx.x;
-	if ((row>=A.height) || (col>=B.width))
+	if ((row>=A.height) || (col>=B.WIDTH))
 	{
 		return;
 	}
-	for (int e=0; e<A.width; e++){
-		Cvalue += A.elements[row*A.width + e] * B.elements[e*B.width + col];
-		C.elements[row*C.width + col] = Cvalue;
+	for (int e=0; e<A.WIDTH; e++){
+		Cvalue += A.elements[row*A.WIDTH + e] * B.elements[e*B.WIDTH + col];
+		C.elements[row*C.WIDTH + col] = Cvalue;
 	}
 }
 
 //square matrix multiplication cpu
 void MatMulCPU(Matrix A, Matrix B, Matrix C)
 {
-	int i,j,k, n = A.width;
+	int i,j,k, n = A.WIDTH;
 	for(i = 0; i < n; ++i){
 		for(j = 0; j < n; ++j)
 		{
-			C.elements[i*C.width +j] = 0;
+			C.elements[i*C.WIDTH +j] = 0;
 			for(k = 0; k < n; ++k)
             {
-                C.elements[i*C.width +j] += A.elements[i*A.width +k] * B.elements[k*A.width +j];
+                C.elements[i*C.WIDTH +j] += A.elements[i*A.WIDTH +k] * B.elements[k*A.WIDTH +j];
             }
 		} 
 	}
@@ -88,33 +88,51 @@ void MatMulCPU(Matrix A, Matrix B, Matrix C)
 
 int main(int argc, char * const argv[])
 {	
-	int Width = 16;
+	if (argc != 4)
+    {
+        fprintf(stderr, "You have to provide WIDTH, ARRAY_A, ARRAY_B  as arguments.\n");
+        return -1;
+    }
+    
+    char *p;
+
+    int WIDTH;
+    WIDTH = strtoul(argv[1], &p, 10);
+
+    std::string ARRAY_A;
+    ARRAY_A = argv[2];
+
+    std::string ARRAY_B;
+	ARRAY_B = argv[3];
 	
+	StopWatchInterface *timer=NULL;
+    sdkCreateTimer(&timer);
+		
 	Matrix A;
 	Matrix B;
 	Matrix C_gpu;
 	Matrix C_cpu;
 	
-	A.width = Width;
-	B.width = Width;
-	C_gpu.width = Width;
-	C_cpu.width = Width;
+	A.WIDTH = WIDTH;
+	B.WIDTH = WIDTH;
+	C_gpu.WIDTH = WIDTH;
+	C_cpu.WIDTH = WIDTH;
 	
-	A.height = Width;
-	B.height = Width;
-	C_gpu.height = Width;
-	C_cpu.height = Width;
+	A.height = WIDTH;
+	B.height = WIDTH;
+	C_gpu.height = WIDTH;
+	C_cpu.height = WIDTH;
 	
-	A.elements = new float[Width*Width];
-	B.elements = new float[Width*Width];
-	C_gpu.elements = new float[Width*Width];
-	C_cpu.elements = new float[Width*Width];
+	A.elements = new float[WIDTH*WIDTH];
+	B.elements = new float[WIDTH*WIDTH];
+	C_gpu.elements = new float[WIDTH*WIDTH];
+	C_cpu.elements = new float[WIDTH*WIDTH];
 	
 	//fill matrices
 	std::ifstream A_input;
 	std::ifstream B_input;
-	A_input.open("A.txt");
-	B_input.open("B.txt");
+	A_input.open(ARRAY_A);
+	B_input.open(ARRAY_B);
 	
 	float a, b;
 	A_input >> a;	
@@ -130,23 +148,33 @@ int main(int argc, char * const argv[])
 	A_input.close();
 	B_input.close();
 
+	sdkResetTimer(&timer);
+    sdkStartTimer(&timer);
 	MatMul(A, B, C_gpu);
+    sdkStopTimer(&timer);
+	float time_gpu = sdkGetTimerValue(&timer);
+	
+	sdkResetTimer(&timer);
+    sdkStartTimer(&timer);
 	MatMulCPU(A, B, C_cpu);
+	float time_cpu = sdkGetTimerValue(&timer);
 
 	//check if the same
-	for (int i=0; i<Width; i++)
-	{	for (int j=0; j<Width; j++)
-			if (C_gpu.elements[i*Width+j] != C_cpu.elements[i*Width+j]){
+	for (int i=0; i<WIDTH; i++)
+	{	for (int j=0; j<WIDTH; j++)
+			if (C_gpu.elements[i*WIDTH+j] != C_cpu.elements[i*WIDTH+j]){
 				return 0;
 			}
 	}
 
 	std::ofstream C_output;
 	C_output.open("C.txt");
-	for (int i=0; i<Width; i++)
-	{	for (int j=0; j<Width; j++)
-			C_output<<C_cpu.elements[i*Width+j]<<"\t";
+	for (int i=0; i<WIDTH; i++)
+	{	for (int j=0; j<WIDTH; j++)
+			C_output<<C_cpu.elements[i*WIDTH+j]<<"\t";
 		C_output<<endl;
 	}
 
+	printf ("%d, %f, %f\n", WIDTH, time_gpu, time_cpu);
+	sdkDeleteTimer(&timer);
 }	
